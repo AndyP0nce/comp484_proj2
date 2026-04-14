aab02b284271f3237ecaaa1a9bb6a7b089625d59
Andrew Ponce <andycool12323@gmail.com>
2026-04-14 16:43:06 -0700

    basic instructions complete

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ $(function() { // Makes sure that your function is called once all the DOM eleme
     $('.treat-button').click(clickedTreatButton);
     $('.play-button').click(clickedPlayButton);
     $('.exercise-button').click(clickedExerciseButton);
+    $('.nap-button').click(clickedPlayButton);
   
 
   
@@ -14,23 +15,41 @@ $(function() { // Makes sure that your function is called once all the DOM eleme
   })
   
     // Add a variable "pet_info" equal to a object with the name (string), weight (number), and happiness (number) of your pet
-    var pet_info = {name:"My Pet Name", weight:"??", happiness:"??"};
+    var pet_info = {name:"Bacon", weight:120, happiness:8};
   
     function clickedTreatButton() {
       // Increase pet happiness
+      pet_info.happiness += 1;
       // Increase pet weight
+      pet_info.weight += 3;
+      $('.pet-message').text("Yum! That was delicious! Woof!");
       checkAndUpdatePetInfoInHtml();
     }
     
     function clickedPlayButton() {
       // Increase pet happiness
+      pet_info.happiness += 2;
       // Decrease pet weight
+      pet_info.weight -= 1;
+      $('.pet-message').text("Woof! That was so fun! Let's do it again!");
       checkAndUpdatePetInfoInHtml();
     }
     
     function clickedExerciseButton() {
       // Decrease pet happiness
+      pet_info.happiness -= 2;
       // Decrease pet weight
+      pet_info.weight -= 3;
+      $('.pet-message').text("*panting* I need a nap... That was exhausting!");
+
+      checkAndUpdatePetInfoInHtml();
+    }
+
+    function clickedNapButton() {
+      // Decrease pet happiness
+      pet_info.happiness -= 1;
+      pet_info.energy += 5;
+      $('.pet-message').text("Zzz... I feel refreshed! Let's play!");
       checkAndUpdatePetInfoInHtml();
     }
   
@@ -41,6 +60,16 @@ $(function() { // Makes sure that your function is called once all the DOM eleme
     
     function checkWeightAndHappinessBeforeUpdating() {
       // Add conditional so if weight is lower than zero.
+      if (pet_info.weight < 0) {
+        pet_info.weight = 0;
+      }
+      // Add conditional so if happiness is lower than zero.
+      if (pet_info.happiness < 0) {
+        pet_info.happiness = 0;
+      }
+        if (pet_info.energy < 0) {
+          pet_info.energy = 0;
+        }
     }
     
     // Updates your HTML with the current values in your pet_info object
@@ -48,5 +77,6 @@ $(function() { // Makes sure that your function is called once all the DOM eleme
       $('.name').text(pet_info['name']);
       $('.weight').text(pet_info['weight']);
       $('.happiness').text(pet_info['happiness']);
+      $('.energy').text(pet_info['energy']);
     }
   
